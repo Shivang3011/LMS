2d6eb61c2316889440464d1fcfcd90964ad98bed
Shivang3011 <68562674+Shivang3011@users.noreply.github.com>
2020-08-31 12:39:30 +0530

    Add files via upload
    Adding some files as well as some new changes

--- LMS/test_admin.py
+++ LMS/test_admin.py
@@ -17,18 +17,25 @@ def setUp():
 def test_launch_application(setUp):
     driver.get("https://node-library-service.herokuapp.com/")
     driver.find_element_by_link_text("Sign-In as Admin").click()
+    driver.get_screenshot_as_file("admin" + 'launch_app.png')
+    driver.save_screenshot(r'C:\Users\Owner\PycharmProjects\Test1\LMS\Screenshots')
 
 
 def test_admin_login(setUp):
     driver.find_element_by_xpath("//input[@placeholder='Enter Username']").send_keys("admin")
     driver.find_element_by_xpath("//input[@placeholder='Enter Password']").send_keys("123")
     driver.find_element_by_xpath("//button[text()='Login']").click()
+    driver.get_screenshot_as_file("admin" + 'login.png')
+    driver.save_screenshot(r'C:\Users\Owner\PycharmProjects\Test1\LMS\Screenshots')
 
 
 def test_manage_books(setUp):
     driver.find_element_by_id("manage_page").click()
+    driver.get_screenshot_as_file("admin" + 'manage_books.png')
+    driver.save_screenshot(r'C:\Users\Owner\PycharmProjects\Test1\LMS\Screenshots')
 
 
+@pytest.mark.skip(reason="will execute this method when needed")
 def test_add_book(setUp):
     driver.find_element_by_xpath("//span[text()='Add New Book']").click()
     driver.find_element_by_id("title").send_keys("test")
@@ -36,26 +43,32 @@ def test_add_book(setUp):
     driver.find_element_by_id("description").send_keys("test")
     driver.find_element_by_id("stock").send_keys("test")
     driver.find_element_by_xpath("//div[@id='addEmployeeModal']//input[@class='btn btn-success']").click()
+    driver.get_screenshot_as_file("admin" + 'add_books.png')
+    driver.save_screenshot(r'C:\Users\Owner\PycharmProjects\Test1\LMS\Screenshots')
 
 
-@pytest.mark.skip(reason="not execute")
+@pytest.mark.skip(reason="will execute this method when needed")
 def test_edit_book(setUp):
     driver.find_element_by_xpath("//div[@id='manage']//tr[1]//td[5]//a[1]//i[1]").click()
 
 
-@pytest.mark.skip(reason="not execute")
+@pytest.mark.skip(reason="will execute this method when needed")
 def test_delete_book(setUp):
     driver.find_element_by_xpath("//div[@id='manage']//tr[1]//td[5]//a[2]//i[1]").click()
 
 
 def test_issue_requests(setUp):
     driver.find_element_by_id("requests_page").click()
+    driver.get_screenshot_as_file("admin" + 'issue_request.png')
+    driver.save_screenshot(r'C:\Users\Owner\PycharmProjects\Test1\LMS\Screenshots')
 
 
-@pytest.mark.skip(reason="not execute")
+@pytest.mark.skip(reason="will execute this method when needed")
 def test_approve_requests(setUp):
     driver.find_element_by_xpath("//div[@id='requests']//a[1]//i[1]").click()
 
 
 def test_reject_requests(setUp):
     driver.find_element_by_xpath("//div[@id='requests']//a[2]//i[1]").click()
+    driver.get_screenshot_as_file("admin" + 'reject_rqst.png')
+    driver.save_screenshot(r'C:\Users\Owner\PycharmProjects\Test1\LMS\Screenshots')
--- LMS/test_user.py
+++ LMS/test_user.py
@@ -17,9 +17,11 @@ def setup():
 def test_launch_application(setup):
     driver.get("https://node-library-service.herokuapp.com/")
     driver.find_element_by_link_text("Sign-In as User").click()
+    driver.get_screenshot_as_file('launch_app.png')
+    driver.save_screenshot(r'C:\Users\Owner\PycharmProjects\Test1\LMS\Screenshots')
 
 
-@pytest.mark.skip(reason="will not execute") # this method will only execute when we remove SKIP
+@pytest.mark.skip(reason="will not execute")  # this method will only execute when we remove SKIP
 def test_user_register(setup):
     driver.find_element_by_link_text("Register").click()
     driver.find_element_by_id("firstName").send_keys("test")  # firstname
@@ -35,11 +37,30 @@ def test_login_as_user(setup):
     driver.find_element_by_xpath("//input[@placeholder='Enter Username']").send_keys("test")
     driver.find_element_by_xpath("//input[@placeholder='Enter Password']").send_keys("123")
     driver.find_element_by_xpath("//button[text()='Login']").click()
+    driver.get_screenshot_as_file('user_login.png')
+    driver.save_screenshot(r'C:\Users\Owner\PycharmProjects\Test1\LMS\Screenshots')
 
 
 def test_browse_books(setup):
     driver.find_element_by_link_text("Browse Books").click()
+    driver.get_screenshot_as_file('browse.png')
+    driver.save_screenshot(r'C:\Users\Owner\PycharmProjects\Test1\LMS\Screenshots')
 
 
 def test_issue_book(setup):
     driver.find_element_by_xpath("//div[5]//div[2]//a[1]").click()
+    driver.get_screenshot_as_file('book_issue.png')
+    driver.save_screenshot(r'C:\Users\Owner\PycharmProjects\Test1\LMS\Screenshots')
+
+
+def test_book_details(setUp):
+    driver.find_element_by_xpath("//body//div[@class='row']//div[@class='row']//div[1]//div[2]//a[2]").click()
+    driver.get_screenshot_as_file('book_details.png')
+    driver.save_screenshot(r'C:\Users\Owner\PycharmProjects\Test1\LMS\Screenshots')
+
+
+def test_activities(setUp):
+    driver.find_element_by_id("activities_page").click()
+    # driver.find_element_by_link_text("Delete all activities").click()
+    driver.get_screenshot_as_file('activities.png')
+    driver.save_screenshot(r'C:\Users\Owner\PycharmProjects\Test1\LMS\Screenshots')
